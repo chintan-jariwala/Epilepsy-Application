6e43bd156a7a0231898cb538d2a9bcde2f304083
Chintan Jariwala <chintanallstarz@gmail.com>
2016-10-31 13:30:09 -0700

    Added a splash Screen and connected the button after the merge

--- app/src/main/java/ser593/com/epilepsy/MainActivity.java
+++ app/src/main/java/ser593/com/epilepsy/MainActivity.java
@@ -1,13 +1,32 @@
 package ser593.com.epilepsy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnPatternComparision = null;
+    Button btnFingerTapping = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPatternComparision = (Button) findViewById(R.id.btnPatternComparision);
+        btnPatternComparision.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnPatternComparision:
+                startActivity(new Intent(this,PatternComparisonProcessingActivity.class));
+                break;
+        }
     }
 }
---
+++ app/src/main/java/ser593/com/epilepsy/SplashActivity.java
@@ -0,0 +1,22 @@
+package ser593.com.epilepsy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by chint on 10/31/2016.
+ */
+
+public class SplashActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
