588e3b76bbcc85c687684b164f10fb7016258d01
MJ <mlin32@asu.edu>
2017-01-23 14:33:26 -0700

    Flanker UI Setup

---
+++ app/src/main/java/ser593/com/epilepsy/UserTasks/Flanker.java
@@ -0,0 +1,16 @@
+package ser593.com.epilepsy.UserTasks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ser593.com.epilepsy.R;
+import ser593.com.epilepsy.Results.ResultActivity;
+
+public class Flanker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flanker);
+    }
+}
